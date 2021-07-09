b8a3f587b6c32e218cc603c357f2ef68883fffdb
Stuart <stuart@projectsuperba.com>
2021-07-09 16:26:24 -0400

    Add prometheus template package

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gitlab.com/innovoedge/components/alertmanager-webhook
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/prometheus/alertmanager v0.22.2
+)
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/alertmanager/template"
 )
 
 func main() {
@@ -17,8 +21,17 @@ func main() {
 }
 
 func handleAlert(c *gin.Context) {
+	// process request data
+	data := &template.Data{}
+	if err := json.NewDecoder(c.Request.Body).Decode(&data); err != nil {
+		c.String(400, "Bad Input")
+		return
+	}
+
 	// ensure JWT is present, fetch/refresh if needed
 
 	// POST new alert payload to /alerting/webhook
+	fmt.Printf("%+v\n", data)
 
+	c.String(200, "Ok")
 }
